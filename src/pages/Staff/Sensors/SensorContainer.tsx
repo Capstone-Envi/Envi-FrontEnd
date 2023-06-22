9c12fa4aa887628f9563e214a60719beb973b24e
thinhdev3820 <thinhdinh.dev.3820@gmail.com>
2023-06-22 14:34:41 +0700

    node detail intergrate api

--- src/pages/Staff/Sensors/SensorContainer.tsx
+++ src/pages/Staff/Sensors/SensorContainer.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import styles from 'src/utils/style';
+
+// ** pages
+import SensorDashboard from './components/SensorDashhboard/SensorDashboard';
 
 const SensorContainer = () => {
-  return <div>SensorContainer</div>;
+  return (
+    <div className={`${styles.flexCenter} font-poppins w-full`}>
+      <div className={`${styles.container}`}>
+        {/* <Suspense fallback={<Loading />}> */}
+        <Routes>
+          <Route path="/">
+            <Route index element={<SensorDashboard />} />
+            <Route path="/:id" element={<SensorDashboard />} />
+          </Route>
+        </Routes>
+        {/* </Suspense> */}
+      </div>
+    </div>
+  );
 };
 
 export default SensorContainer;
